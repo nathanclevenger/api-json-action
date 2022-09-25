a3e93d6772625c6716b1738b3241c2478c7cf102
Nathan Clevenger <nateclev@gmail.com>
2022-09-25 14:57:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import core from '@actions/core'
 import github from '@actions/github'
+import yaml from 'yaml'
 
 try {
   // const nameToGreet = core.getInput('who-to-greet')
@@ -8,8 +9,24 @@ try {
   const time = (new Date()).toTimeString()
   // core.setOutput("time", time)
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  fs.writeFileSync('api.json', payload)
+  const config = yaml.parse('_config.yaml')
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+  fs.writeFileSync('api.json', JSON.stringify({
+    icon: '入',
+    name: 'lambda.do',
+    description: 'Instant Globally-distributed Lambda Functions as APIs',
+    url: 'https://lambda.to/api',
+    type: 'https://apis.do/lambda',
+    endpoints: {
+      listSources: 'https://lambda.to/sources',
+    },
+    site: 'https://lambda.to',
+    login: 'https://lambda.to/login',
+    signup: 'https://lambda.to/signup',
+    subscribe: 'https://lambda.to/subscribe',
+    repo: 'https://github.com/nathanclevenger/lambda.to',
+    config,
+  }, null, 2))
   console.log(`The event payload: ${payload}`)
 } catch (error) {
   core.setFailed(error.message)
